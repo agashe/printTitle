0fcae6982e52cc22a13badafa2ae64fef69868db
Mohamed Yousef <modi401@hotmail.com>
2020-07-31 16:34:52 +0200

    print litters

---
+++ demo.c
@@ -0,0 +1,8 @@
+#include "printTitle.h"
+
+int main(){
+    char *x = "vwxyz";
+    printTitle(x, x, '@');
+
+    return 0;
+}
---
+++ printTitle.h
@@ -0,0 +1,22 @@
+/**
+ * printTitle v1.0.0
+ *
+ * Auther: Mohamed Yousef <engineer.mohamed.yousef@gmail.com>
+ * Copyrights: printTitle (C) 2020
+ * License: MIT
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Draw litters on the screen.
+ *
+ * @param string text
+ * @param string decoration
+ * @param char   block
+ *
+ * @return void
+ */
+void printTitle(char *text, char *decoration, char block);
